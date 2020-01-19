dbe2427c15d2fd0b83db1e619db6053bf6ea634a
Scott Young <scott@sjy.id.au>
2020-01-19 15:44:40 +1100

    remove obsolete #ifdef

--- keymap.c
+++ keymap.c
@@ -2,7 +2,6 @@
 
 // Macros to simplify the layout of this file; allows for keys on the left and
 // right hands to be defined separately rather than interleaved.
-#define ___ _______
 #define LR_LAYOUT( \
     k01, k02, k03, k04, k05,   k06, k07, k08, k09, k10,         \
     k11, k12, k13, k14, k15,   k16, k17, k18, k19, k20, k21,    \
